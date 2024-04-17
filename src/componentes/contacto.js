8fb6522bb2cc07bd7449fe7e1fe61f64e0c45a88
Abyss0108 <linamaximo123@gmail.com>
2024-04-17 08:17:53 -0600

    Contacto y Especialistas
    Contacto y Especialistas

--- src/componentes/contacto.js
+++ src/componentes/contacto.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Clin from '../img/clin.jpg';
+import contac from '../img/contac.png';
+import direcc from '../img/direcc.png';
 
 export default function Contactoo(){
     return(
@@ -8,8 +10,8 @@ export default function Contactoo(){
             <h1 className="text-center">Contacto</h1>
             <div className="row p-3 border mb-3">
                     <div className="col-md-9">
-                        <h3>Lorem ipsum dolor sit amet</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit aenean ante, sollicitudin arcu sociis vestibulum nostra nam non penatibus ullamcorper, lectus fringilla mollis viverra scelerisque ultrices nulla pharetra. Aliquam justo vestibulum fringilla turpis phasellus mattis nisl dapibus vitae nulla, socios feugiat bibendum dignissim accumsan luctus sem consequat velit. Himenaeos tincidunt euismod ultricies libero cursus mi sodales vestibulum eleifend, purus nisi dis sem facilisi facilisis penatibus class pellentesque, at fermentum aliquam nibh phasellus turpis habitant socios.
+                        <h3>Para atenderte con el mejor servicio</h3>
+                        <p>Estamos para atenderte con la mejor atención especializada en pie diabético. Nuestro equipo de expertos está listo para ayudarte en cada paso del camino. Ya sea que necesites información adicional, programar una cita o resolver cualquier inquietud, no dudes en contactarnos. Estamos aquí para brindarte el cuidado que necesitas para mantener tus pies sanos y tu calidad de vida en óptimas condiciones.
                         </p>
                     </div>
             </div>
@@ -21,13 +23,16 @@ export default function Contactoo(){
                                
                                 <div className="col-md-8">
                                     <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <h5 className="card-title">Direccion</h5>
+                                        <p className="card-text">C. Cuauhtémoc 18, Capitán Antonio Reyes,
+                                    </p>
+                                    <p className="card-text">43000 Huejutla de Reyes, Hgo.
+                                    </p>
                                         <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                                     </div>
                                 </div>
                                 <div className="col-md-4">
-                                    <img src={Clin} className="img-fluid rounded-start" alt="..." />
+                                    <img src={direcc} className="img-fluid rounded-start" alt="..." />
                                 </div>
                             </div>
                         </div>
@@ -37,13 +42,16 @@ export default function Contactoo(){
                             <div className="row g-0">
                                 <div className="col-md-8">
                                     <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <h5 className="card-title">Contacto</h5>
+                                        <p className="card-text">Telefono: +52 666 2222 999       
+                                        </p>
+                                        <p className="card-text">Correo: info@cirupied.com
+                                        </p>
                                         <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                                     </div>
                                 </div>
                                 <div className="col-md-4">
-                                    <img src={Clin} className="img-fluid rounded-start" alt="..." />
+                                    <img src={contac} className="img-fluid rounded-start" alt="..." />
                                 </div>
                             </div>
                         </div>
