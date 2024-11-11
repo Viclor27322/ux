b9680055e58b6537c9dd8e51aff124c978dc844c
Viclor27 <vito27322@gmail.com>
2024-11-11 17:54:22 -0600

    aaaaa

--- src/CSP/CspMetaTag.js
+++ src/CSP/CspMetaTag.js
@@ -6,7 +6,7 @@ export function CSPMetaTag() {
     meta.httpEquiv = 'Content-Security-Policy';
     meta.content = `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' https://js.stripe.com https://m.stripe.network https://apis.google.com https://www.gstatic.com https://www.google.com https://cdn.jsdelivr.net;
+       script-src 'self' 'sha256-5+YTmTcBwCYdJ8Jetbr6kyjGp0Ry/H7ptpoun6CrSwQ=' 'sha256-/5Guo2nzv5n/w6ukZpOBZOtTJBJPSkJ6mhHpnBgm3Ls=' https://js.stripe.com https://m.stripe.network;
       style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net/npm/antd@5.11.3/dist/antd.css;
       font-src 'self' https://cdn.jsdelivr.net/npm/antd@5.11.3/dist/fonts/;
       img-src 'self' blob: data: https://res.cloudinary.com;
