ff315b1533ae25c6ce58446cc68ca80d24dda11a
Viclor27 <vito27322@gmail.com>
2024-11-11 16:47:06 -0600

    update sprite meta

--- src/CSP/CspMetaTag.js
+++ src/CSP/CspMetaTag.js
@@ -6,11 +6,11 @@ export function CSPMetaTag() {
     meta.httpEquiv = 'Content-Security-Policy';
     meta.content = `
       default-src 'self';
-      script-src 'self' https://apis.google.com https://www.gstatic.com https://www.google.com https://cdn.jsdelivr.net https://js.stripe.com;
+      script-src 'self' 'unsafe-inline' https://apis.google.com https://www.gstatic.com https://www.google.com https://cdn.jsdelivr.net https://js.stripe.com;
       style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net/npm/antd@5.11.3/dist/antd.css;
       font-src 'self' https://cdn.jsdelivr.net/npm/antd@5.11.3/dist/fonts/;
       img-src 'self' blob: data: https://res.cloudinary.com;
-      connect-src 'self' https://www.google-analytics.com https://rest-api2-three.vercel.app https://newapiimagenes.onrender.com https://api.stripe.com;
+      connect-src 'self' https://www.google-analytics.com https://rest-api2-three.vercel.app https://newapiimagenes.onrender.com https://api.stripe.com https://m.stripe.network;
       frame-src 'self' https://proyecto-accr.onrender.com https://www.google.com.mx https://js.stripe.com;
       form-action 'self';
     `;
